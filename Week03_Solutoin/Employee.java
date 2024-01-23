d00eb97d401001695340520f6bc597a0ae9c6a0d
mabbasiazad <m32abbas@uwaterloo.ca>
2024-01-23 09:49:41 -0500

    binary search for employee

---
+++ Week03_Solutoin/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    public int id;
+
+    public Employee(int id) {
+        this.id  = id;
+    }
+}
--- Week03_Solutoin/RecursiveBinarySearch.java
+++ Week03_Solutoin/RecursiveBinarySearch.java
@@ -1,9 +1,17 @@
 public class RecursiveBinarySearch {
     public static void main(String[] args) {
         // Example usage:
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int key = 8;
-        int result = recursiveBinarySearch(array, key);
+        //int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Employee[] employees = new Employee[4]; 
+
+        Employee employee_0 = new Employee(123);
+        Employee employee_1 = new Employee(2354);
+        
+        employees[0] = employee_0;
+        employees[1] = employee_1;
+        
+        int key_id = 2354;
+        int result = recursiveBinarySearch(employees, key_id);
 
         if (result != -1) {
             System.out.println("Key found at index: " + result);
@@ -13,17 +21,17 @@ public class RecursiveBinarySearch {
     }
 
     // Recursive Binary Search function
-    static int recursiveBinarySearch(int[] arr, int key) {
+    static int recursiveBinarySearch(Employee[] arr, int key) {
         return binarySearchIter(arr, key, 0, arr.length - 1);
     }
 
-    static int binarySearchIter(int[] arr, int key, int low, int high) {
+    static int binarySearchIter(Employee[] arr, int key, int low, int high) {
         if (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if (arr[mid] == key) {
+            if (arr[mid].id == key) {
                 return mid; // Key found
-            } else if (arr[mid] > key) {
+            } else if (arr[mid].id > key) {
                 return binarySearchIter(arr, key, low, mid - 1); // Search in the left half
             } else {
                 return binarySearchIter(arr, key, mid + 1, high); // Search in the right half
